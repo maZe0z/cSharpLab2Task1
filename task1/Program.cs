a6bb7ef16b17252314447860861143454d1a170a
Zakhar <38567289+maZe0z@users.noreply.github.com>
2021-03-01 19:36:02 +0300

    :)

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZString zString = new ZString();
+            
+            Console.WriteLine("Enter a string to reverse:");
+
+            zString.InitialString = Console.ReadLine();
+            
+            zString.ReverseString();
+            
+            zString.ShowString();
+        }
+    }
+}
---
+++ task1/ZString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task1
+{
+    public class ZString
+    {
+        public string InitialString { get; set; }
+
+        private string[] Words { get; set; }
+        
+        public void ReverseString()
+        {
+            Words = InitialString.Split(' ');
+
+            Array.Reverse(Words);
+        }
+
+        public void ShowString()
+        {
+            if (Words.Length == 1)
+            {
+                Console.WriteLine("There is only one word or the string is empty!");
+            }
+            else
+            {
+                foreach (var word in Words)
+                {
+                    Console.Write(word + " ");
+                }
+            }
+        }
+    }
+}
